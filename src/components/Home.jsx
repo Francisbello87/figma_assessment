4f81a04e188699219bd3e46c0cb22cc2def29d4f
Francis Bello <bellofrancis87@gmail.com>
2023-05-22 17:12:22 +0100

    fixed the bug on the cta button, my PC went off without any reason, hence late submission

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -44,9 +44,9 @@ const Home = () => {
           </div>
         </div>
       ) : (
-        <div className="flex w-full h-full  justify-center  flex-col items-center ">
-          <div className="flex  h-full  text-center justify-center items-center flex-col ">
-            <h1 className="font-bold w-full text-center font-HelveticaBold  text-2xl md:text-4xl mt-10 text-[#240D57] mb-4">
+        <div className="flex w-full h-full  flex-col items-center ">
+          <div className="flex  h-full  text-center  items-center flex-col ">
+            <h1 className="font-bold text-center font-HelveticaBold  text-3xl md:text-7xl mt-10 text-[#240D57] mb-4">
               Imagine if <br />{" "}
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#8456EC] to-[#E87BF8]">
                 Snapchat
@@ -54,17 +54,17 @@ const Home = () => {
               <br />
               had events.
             </h1>
-            <p className="text-[#4f4f4f] text-xl w-full md:text-2xl font-Helvetica">
+            <p className="text-[#4f4f4f] text-sm w-[300px] md:w-[600px] px-4 md:text-2xl font-Helvetica">
               Easily host and share events with your friends <br />
               across any social media
             </p>
-            <div>
-            <motion.img whileInView={{opacity: [0, 1]}} transition={{duration: 0.8}} className="md:h-full h-[290px]" src={Phone} alt="Phone" />
+            <div className="md:h-[548px]">
+            <motion.img whileInView={{opacity: [0, 1]}} transition={{duration: 0.8}} className="md:h-full h-[290px] " src={Phone} alt="Phone" />
           </div>
             <motion.button
               whileTap={{ scale: 1.1 }}
               transition={{ duration: 0.2, ease: "easeInOut" }}
-              className=" hover:drop-shadow-md   hover:bg-gradient-to-l from-[#E87BF8]   to-[#8456EC]  text-xs md:text-xl font-Helvetica bg-gradient-to-r from-[#8456EC] to-[#E87BF8] md:py-8 md:px-24 px-24 py-2 w-[100%] text-center rounded-[10px] md:my-14 "
+              className=" hover:drop-shadow-md flex items-center justify-center md:text-2xl  hover:bg-gradient-to-l from-[#E87BF8]   to-[#8456EC]  text-xs  font-Helvetica bg-gradient-to-r from-[#8456EC] to-[#E87BF8] md:py-4 md:px-8 px-4 py-2 w-[150px] md:w-[70%] text-left rounded-[10px] md:my-10 "
             >
               🎉 Create my event
             </motion.button>
